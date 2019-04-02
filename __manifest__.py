10bf59e223c0cb788c4423912521eb2038a99175
The256support <44253802+The256support@users.noreply.github.com>
2019-04-02 16:39:02 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Report Inventory',
+    'version': '12.0.1.0.0',
+    'summary': 'Report Inventory',
+    'author': 'The256',
+    'maintainer': 'The256',
+    'company': 'The256',
+    'website': 'https://the256.pro/',
+    'depends': ['stock'],
+    'demo': [],
+    'data': [
+		'views/stock.report_inventory.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
